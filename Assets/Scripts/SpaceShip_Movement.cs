1b545291857017f0226d5daf8f2e105eda723c32
I-AM-INSANE <vasiliyqqerty@gmail.com>
2021-05-05 01:19:31 +0300

    Update

---
+++ Assets/Scripts/SpaceShip_Movement.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShip_Movement : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField] private float boost;
+    private Rigidbody rb;
+    private float verticalAxisValue;
+    private float horizontalAxisValue;
+    private float jumpAxisValue;
+
+    #endregion
+
+    #region Methods
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        verticalAxisValue = Input.GetAxis("Vertical");
+        horizontalAxisValue = Input.GetAxis("Horizontal");
+        jumpAxisValue = Input.GetAxis("Jump");
+        Move();
+    }
+
+    private void Move()
+    {
+        if (verticalAxisValue != 0)
+        {
+            transform.Rotate(new Vector3(0, 0, 1) * verticalAxisValue);
+        }
+        if (horizontalAxisValue != 0)
+        {
+            transform.Rotate(new Vector3(0, 0, 1) * horizontalAxisValue);
+        }
+        if (jumpAxisValue != 0)
+        {
+            rb.AddRelativeForce(Vector3.up * boost);
+        }
+    }
+
+    #endregion
+}
